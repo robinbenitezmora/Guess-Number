fdd1afe6aa0302ccb39b5b7b385d6d4b8d8c431f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-25 11:45:59 -0500

    Add 'createGUI' method

--- Play.java
+++ Play.java
@@ -1,6 +1,12 @@
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 class Play extends JPanel {
  final Game game;
@@ -18,5 +24,20 @@ class Play extends JPanel {
   BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
   this.setLayout(boxlayout);
   createGUI();
+ }
+
+ private void createGUI() {
+  JLabel playScore;
+  JLabel gameText;
+  JLabel mysteryNumber;
+  JLabel statusImage;
+
+  playScore = new JLabel(
+    "Score: " + scoreFiles.intScore("current_score.txt") + " Games: " + scoreFiles.intScore("num_game.txt"));
+  playScore.setFont(new Font("Arial", Font.PLAIN, 20));
+  playScore.setForeground(Color.WHITE);
+  playScore.setAlignmentX(CENTER_ALIGNMENT);
+  playScore.setBorder(new EmptyBorder(10, 10, 10, 10));
+  this.add(playScore);
  }
 }
