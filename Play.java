3e3667cd408e14ee760bc6f88e2bc2eace562297
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-25 13:04:51 -0500

    Correct 'playScore' method

--- Play.java
+++ Play.java
@@ -7,37 +7,38 @@ import javax.swing.border.EmptyBorder;
 
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import java.awt.Font;
 
 class Play extends JPanel {
- final Game game;
+  final Game game;
 
- RandomNumber randomNumber = new RandomNumber();
+  RandomNumber randomNumber = new RandomNumber();
 
- ScoringSystem scoringSystem = new ScoringSystem();
+  ScoringSystem scoringSystem = new ScoringSystem();
 
- ScoreFiles scoreFiles = new ScoreFiles();
+  ScoreFiles scoreFiles = new ScoreFiles();
 
- public Play(Game game) {
-  this.game = game;
+  public Play(Game game) {
+    this.game = game;
 
-  // Layout to be used in this panel
-  BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
-  this.setLayout(boxlayout);
-  createGUI();
- }
+    // Layout to be used in this panel
+    BoxLayout boxlayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+    this.setLayout(boxlayout);
+    createGUI();
+  }
 
- private void createGUI() {
-  JLabel playScore;
-  JLabel gameText;
-  JLabel mysteryNumber;
-  JLabel statusImage;
+  private void createGUI() {
+    JLabel playScore;
+    JLabel gameText;
+    JLabel mysteryNumber;
+    JLabel statusImage;
 
-  playScore = new JLabel(
-    "Score: " + scoreFiles.intScore("current_score.txt") + " Games: " + scoreFiles.intScore("num_game.txt"));
-  playScore.setFont(new Font("Arial", Font.PLAIN, 20));
-  playScore.setForeground(Color.WHITE);
-  playScore.setAlignmentX(CENTER_ALIGNMENT);
-  playScore.setBorder(new EmptyBorder(10, 10, 10, 10));
-  this.add(playScore);
- }
+    playScore = new JLabel(
+        "Score: " + scoreFiles.intScore("current_score.txt") + " Games: " + scoreFiles.intScore("num_game.txt"));
+    playScore.setFont(new java.awt.Font("link Free", java.awt.Font.BOLD, 30));
+    playScore.setForeground(new java.awt.Color(253, 233, 180));
+    playScore.setAlignmentX(CENTER_ALIGNMENT);
+    playScore.setBorder(new EmptyBorder(10, 10, 10, 10));
+    this.add(playScore);
+  }
 }
