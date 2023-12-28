621cbb57788d09bb8d574b99b871528e0cf7209f
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-27 19:25:05 -0500

    Create methods for this class

--- ScoreFiles.java
+++ ScoreFiles.java
@@ -1,8 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ScoreFiles {
 
- public String intScore(String string) {
-  return null;
+ public String showScore(String filename) {
+  String score = "0";
+  try {
+   File file = new File(filename);
+   Scanner scanner = new Scanner(file);
+   while (scanner.hasNextLine()) {
+    score = scanner.nextLine();
+   }
+   scanner.close();
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  }
+  return score;
+ }
+
+ public String showGames(String filename) {
+  String attempt = "0";
+  try {
+   File file = new File(filename);
+   Scanner scanner = new Scanner(file);
+   while (scanner.hasNextLine()) {
+    attempt = scanner.nextLine();
+   }
+   scanner.close();
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  }
+  return attempt;
+ }
+
+ public int intScore(String filename) {
+  return Integer.parseInt(showScore(filename));
+ }
+
+ public int intGames(String filename) {
+  return Integer.parseInt(showGames(filename));
+ }
+
+ public void write(String filename, int score) {
+  try {
+   java.io.FileWriter myWriter = new java.io.FileWriter(filename);
+   myWriter.write(String.valueOf(score));
+   myWriter.close();
+  } catch (java.io.IOException e) {
+   System.out.println("An error occurred.");
+   e.printStackTrace();
+  }
+ }
+
+ public void writeScoreAttempts(String filename, int score, int attempts) {
+  try {
+   java.io.FileWriter myWriter = new java.io.FileWriter(filename);
+   myWriter.write(String.valueOf(score));
+   myWriter.write("\n");
+   myWriter.write(String.valueOf(attempts));
+   myWriter.close();
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+  } catch (java.io.IOException e) {
+   System.out.println("An error occurred.");
+   e.printStackTrace();
+  }
  }
 
 }
